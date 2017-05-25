c845e7e1a085973dca6ea494034df8bd090cb222
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2017-05-25 15:06:37 +0200

    Solve tiny maze

--- tiny-maze/src/tiny_maze/solver.clj
+++ tiny-maze/src/tiny_maze/solver.clj
@@ -9,4 +9,34 @@
   - 1 : This is a wall that you cannot pass through
   - 0 : A free space that you can move through.")
 
-(defn solve-maze [maze])
+
+(defn find-in-maze [k maze]
+  (first (filter #(<= 0 (second %)) (zipmap (range) (map #(.indexOf % k) maze)))))
+
+(defn get-neighbours [position maze]
+  (let [x-size (count maze)
+        y-size (count (first maze))
+        positions [[-1 0] [0 1] [1 0] [0 -1]]]
+    (filter
+      (fn [[a b]] (and (< -1 a x-size) (< -1 b y-size)))
+      (map #(map + position %) positions))))
+
+(defn get-free-neighbours [start maze]
+  (filter #(#{0 :E} (get-in maze %)) (get-neighbours start maze)))
+
+(defn maze->tree [start maze]
+  (if (= (get-in maze start) :E)
+    (assoc-in maze start :x)
+    (mapcat #(maze->tree % (assoc-in maze start :x))
+         (get-free-neighbours start maze))))
+
+(defn path-length [path]
+  (count (filter #{:x} (flatten path))))
+
+(defn sorted-paths [paths]
+  (sort-by path-length paths))
+
+(defn solve-maze [maze]
+  (let [paths (partition (count maze) (maze->tree (find-in-maze :S maze) maze))]
+    (first (sorted-paths paths))))
+
--- tiny-maze/test/tiny_maze/solver_test.clj
+++ tiny-maze/test/tiny_maze/solver_test.clj
@@ -2,6 +2,12 @@
   (:require [clojure.test :refer :all]
             [tiny-maze.solver :refer [solve-maze]]))
 
+(defn biggest-maze [x y] (-> (vec (repeat x (vec (repeat y 0))))
+                             (assoc-in [0 0] :S)
+                             (assoc-in [(dec x) (dec y)] :E)))
+
+(defn solution-maze [x y] (vec (mapv #(assoc % (dec y) :x) (assoc (biggest-maze x y) 0 (vec (repeat x :x))))))
+
 ;; Hint: Start with a smaller maze
 (deftest test-solve-maze
   (testing "can find way to exit with 3x3 maze"
@@ -19,7 +25,23 @@
                 [1  0  0 1]
                 [1  1  0 :E]]
           sol [[:x :x :x 1]
-                [1  1 :x 0]
-                [1  0 :x 1]
-                [1  1 :x :x]]]
-     (is (= sol (solve-maze maze))))))
+               [1  1 :x 0]
+               [1  0 :x 1]
+               [1  1 :x :x]]]
+     (is (= sol (solve-maze maze)))))
+
+  (testing "can find shortest path"
+    (let [maze [[0 :S  0  0  1]
+                [0  1  1  0  1]
+                [0  0  1  0  1]
+                [1  0  0  0 :E]]
+          sol [[0 :x :x :x  1]
+               [0  1  1 :x  1]
+               [0  0  1 :x  1]
+               [1  0  0 :x :x]]]
+      (is (= sol (solve-maze maze)))))
+
+  (testing "the big stuff"
+    (let [x 5]
+      (is (= (solution-maze x x) (solve-maze (biggest-maze x x)))))))
+
